7b021c56453b9d66c7f0d0cc569a193bf8caaaa0
Sangbin <mosangbin94@gmail.com>
2022-03-20 14:48:16 +0900

    알고리즘

---
+++ Inflearn/기본문제/1.js
@@ -0,0 +1,26 @@
+// function solution(a, b, c){
+//     let answer;
+//     if(a < b && a < c) {
+//         answer = a;
+//     } else if ( b < a && b < c) {
+//         answer = b;
+//     } else {
+//         answer = c;
+//     }
+//     return answer;
+// }
+
+// console.log(solution(60, 50, 11));
+
+
+function solution(a, b, c){
+    let answer;
+    if (a < b) answer = a;
+    else answer = b;
+    if (c < answer) answer = c;
+    return answer;
+}
+
+console.log(solution(60, 50, 11));
+
+
