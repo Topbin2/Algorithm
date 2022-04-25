c3dc5afbe08a10b7c0bb800b3cba61678fc25b47
SANGBIN <mosangbin94@gmail.com>
2022-04-25 20:11:19 +0900

    복습

--- Inflearn/문자열 탐색/2.js
+++ Inflearn/문자열 탐색/2.js
@@ -1,9 +1,20 @@
+// function solution(s){
+//   let answer = '';
+//   s = s.toLowerCase().replace(/[^a-z]/g, '');
+//   if(s.split('').reverse().join('') === s) {
+//     return answer = 'YES';
+//   } else return answer = 'NO';
+// }
+
+// let str="found7, time: study; Yduts; emit, 7Dnuof";
+// console.log(solution(str));
+
 function solution(s){
   let answer = '';
   s = s.toLowerCase().replace(/[^a-z]/g, '');
-  if(s.split('').reverse().join('') === s) {
-    return answer = 'YES';
-  } else return answer = 'NO';
+  if(s.split('').reverse().join('') === s) answer = 'YES';
+  else answer = 'NO';
+  return answer;
 }
 
 let str="found7, time: study; Yduts; emit, 7Dnuof";
