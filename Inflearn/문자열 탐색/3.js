03a6b4d54ae1f5773aa90a71a6984a0aa7f4082a
SANGBIN <mosangbin94@gmail.com>
2022-04-25 21:10:12 +0900

    복습

--- Inflearn/문자열 탐색/3.js
+++ Inflearn/문자열 탐색/3.js
@@ -20,11 +20,20 @@
 // let str="g0en2T0s8eSoft12";
 // console.log(solution(str));
 
-function solution(str){
-  str = str.replace(/[^0-9]/g, '');
-  const arr = str.split('');
-  return parseInt(arr.sort((a, b) => a - b).join(''));
-}
+// function solution(str){
+//   str = str.replace(/[^0-9]/g, '');
+//   const arr = str.split('');
+//   return parseInt(arr.sort((a, b) => a - b).join(''));
+// }
 
+function solution(str){
+  let answer = '';
+  for(let x of str) {
+    if(!isNaN(x)) {
+      answer += x;
+    }
+  }
+  return parseInt(answer);
+}
 let str="g0en2T0s8eSoft120123";
 console.log(solution(str));
