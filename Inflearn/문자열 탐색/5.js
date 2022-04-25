88559da6967d28708fa553d5c7a89768411f4fd5
SANGBIN <mosangbin94@gmail.com>
2022-04-25 22:16:10 +0900

    복습

--- Inflearn/문자열 탐색/5.js
+++ Inflearn/문자열 탐색/5.js
@@ -1,13 +1,34 @@
+// function solution(s) {
+//   let answer = "";
+//   let num = 1;
+//   s = s + ' ';
+//   for (let i = 0; i < s.length - 1; i++) {
+//     if(s[i] === s[i + 1]) num++;
+//     else {
+//       answer += s[i];
+//         if(num > 1) answer += num;
+//         num = 1;
+//     }
+//   }
+//   return answer;
+// }
+
+// let str = "KKHSSSSSSSE";
+// console.log(solution(str));
+
 function solution(s) {
   let answer = "";
-  let num = 1;
+  let number = 1;
   s = s + ' ';
   for (let i = 0; i < s.length - 1; i++) {
-    if(s[i] === s[i + 1]) num++;
-    else {
+    if (s[i] === s[i + 1]) {
+      number++;
+    } else {
       answer += s[i];
-        if(num > 1) answer += num;
-        num = 1;
+      if(number > 1) {
+        answer += number;
+        number = 1;
+      }
     }
   }
   return answer;
