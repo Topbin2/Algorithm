e6c33b93ea6f91dcdccbb2ccb0010056a7015e3a
SANGBIN <mosangbin94@gmail.com>
2022-04-25 20:01:50 +0900

    복습

--- Inflearn/문자열 탐색/1.js
+++ Inflearn/문자열 탐색/1.js
@@ -1,18 +1,18 @@
-function solution(s){
-  answer = '';
-  const str = s.toLowerCase();
-  const newStr = str.split('').reverse().join('');
-  console.log(str, newStr);
-  if (str === newStr) {
-    answer = 'YES';
-  } else {
-    answer = 'NO';
-  }
-  return answer;
-}
+// function solution(s){
+//   answer = '';
+//   const str = s.toLowerCase();
+//   const newStr = str.split('').reverse().join('');
+//   console.log(str, newStr);
+//   if (str === newStr) {
+//     answer = 'YES';
+//   } else {
+//     answer = 'NO';
+//   }
+//   return answer;
+// }
 
-let str="gooGa";
-console.log(solution(str));
+// let str="gooGa";
+// console.log(solution(str));
 
 // function solution(s){
 //   let answer = '';
@@ -29,3 +29,15 @@ console.log(solution(str));
 
 // let str="gooG";
 // console.log(solution(str));
+
+function solution(s){
+  answer = '';
+  s = s.toLowerCase();
+  arr = s.split('').reverse().join('');
+  if(s === arr) answer = 'YES';
+  else answer = 'NO';
+  return answer;
+}
+
+let str="gooG";
+console.log(solution(str));
