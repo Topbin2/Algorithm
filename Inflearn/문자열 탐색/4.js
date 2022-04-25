e5f4b9ef54c5ce545082ae2f7ee52252a6369046
SANGBIN <mosangbin94@gmail.com>
2022-04-25 22:03:51 +0900

    복습

--- Inflearn/문자열 탐색/4.js
+++ Inflearn/문자열 탐색/4.js
@@ -26,3 +26,34 @@ function solution(s, t) {
 
 let str = "teachermode";
 console.log(solution(str, "e"));
+
+// function solution(s, t) {
+//   let answer = [];
+//   let p = s.length + 1;
+
+//   for(let x of s) {
+//     if(x === t) {
+//       p = 0;
+//       answer.push(p);
+//     } else {
+//       p++;
+//       answer.push(p);
+//     }
+//   }
+
+//   p = s.length + 1;
+//   for(let i = s.length - 1; i >= 0; i--) {
+//     if(s[i] === t) {
+//       p = 0;
+//       answer[i] = p;
+//     } else {
+//       p++;
+//       answer[i] = Math.min(answer[i], p);
+//     }
+//   }
+
+//   return answer;
+// }
+
+// let str = "teachermode";
+// console.log(solution(str, "e"));
