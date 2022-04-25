de2a629da47af4ab63158037833754854c679c26
SANGBIN <mosangbin94@gmail.com>
2022-04-25 21:05:40 +0900

    복습

--- Inflearn/문자열 탐색/3.js
+++ Inflearn/문자열 탐색/3.js
@@ -7,15 +7,24 @@
 // let str="g0en2T0s8eSoft12";
 // console.log(solution(str));
 
+// function solution(str){
+//   let answer = '';
+//   for(let x of str) {
+//    if(!isNaN(x)) {
+//      answer += x;
+//    }
+//   }
+//   return parseInt(answer);
+// }
+
+// let str="g0en2T0s8eSoft12";
+// console.log(solution(str));
+
 function solution(str){
-  let answer = '';
-  for(let x of str) {
-   if(!isNaN(x)) {
-     answer += x;
-   }
-  }
-  return parseInt(answer);
+  str = str.replace(/[^0-9]/g, '');
+  const arr = str.split('');
+  return parseInt(arr.sort((a, b) => a - b).join(''));
 }
 
-let str="g0en2T0s8eSoft12";
+let str="g0en2T0s8eSoft120123";
 console.log(solution(str));
