3df0d4e9c3f3bba611ee45fb162ab9d3a4913c62
SANGBIN <mosangbin94@gmail.com>
2022-04-26 20:14:20 +0900

    복습

--- Inflearn/완전탐색(블루투포스)/1.js
+++ Inflearn/완전탐색(블루투포스)/1.js
@@ -1,19 +1,45 @@
+// function solution(n, arr){
+//   let answer, max = Number.MIN_SAFE_INTEGER;
+//   for(let x of arr) {
+//     let sum = 0, tmp = x;
+//     while(tmp) {
+//       sum += tmp % 10; // 0 + 8 + 2 + 1
+//       tmp = Math.floor(tmp / 10); // 12, 1, 0 
+//     }
+//     if(sum > max) { //sum = 11, max = 11
+//       max = sum;
+//       answer = x;
+//     }
+//     if(sum === max) {
+//       if(x > answer) answer = x;
+//     }
+//   }
+//   return answer;
+// }
+
+// let arr=[128, 460, 603, 40, 521, 137, 123];
+// console.log(solution(7, arr));
+
 function solution(n, arr){
-  let answer, max = Number.MIN_SAFE_INTEGER;
+  let answer;
+  let max = Number.MIN_SAFE_INTEGER;
+
   for(let x of arr) {
-    let sum = 0, tmp = x;
-    while(tmp) {
-      sum += tmp % 10; // 0 + 8 + 2 + 1
-      tmp = Math.floor(tmp / 10); // 12, 1, 0 
+    let sum = 0;
+    let num = x;
+    while(num) {
+      sum += num % 10;
+      num = Math.floor(num / 10);
     }
-    if(sum > max) { //sum = 11, max = 11
+    if(sum > max) {
+      answer = sum;
       max = sum;
-      answer = x;
     }
     if(sum === max) {
-      if(x > answer) answer = x;
+      if(answer < x) answer = x;
     }
   }
+
   return answer;
 }
 
