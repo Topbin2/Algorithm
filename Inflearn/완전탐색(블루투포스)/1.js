d2e14a5fe8947430aba589fb0dfac07ddbf46f94
SANGBIN <mosangbin94@gmail.com>
2022-04-26 19:35:27 +0900

    1.js

---
+++ Inflearn/완전탐색(블루투포스)/1.js
@@ -0,0 +1,14 @@
+function solution(n, arr){
+  let answer, max = Number.MIN_SAFE_INTEGER;
+  for(let x of arr) {
+    let sum = 0, tmp = x;
+    while(tmp){
+      sum += (tmp % 10); //8
+      tmp = Math.floor(tmp/10) //12
+    }
+    console.log(sum);
+  }
+}
+
+let arr=[128, 460, 603, 40, 521, 137, 123];
+console.log(solution(7, arr));
