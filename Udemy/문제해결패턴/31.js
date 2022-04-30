e3a8e06fb99a1add58f09f4bef0d2b28c4f3c3aa
SANGBIN <mosangbin94@gmail.com>
2022-05-01 00:10:44 +0900

    31

--- Udemy/문제해결패턴/31.js
+++ Udemy/문제해결패턴/31.js
@@ -10,18 +10,29 @@
 //   return result;
 // }
 
+// function solution(arr) {
+//   let result = [];
+
+//   for(let x of arr) {
+//     if(!result.includes(x)) {
+//       result.push(x)
+//     };
+//   }
+
+//   return result.length;
+// }
+
 function solution(arr) {
-  let result = [];
-
-  for(let x of arr) {
-    if(!result.includes(x)) {
-      result.push(x)
-    };
+  if(arr.length === 0) return 0;
+  let i = 0;
+  for(let j = 1; j < arr.length; j++) {
+    if(arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
   }
-
-  return result.length;
+  return i + 1;
 }
-
 
 console.log(solution([1,1,1,1,1,2]));
 console.log(solution([1,2,3,4,4,4,7,7,12,12,13,13]));
