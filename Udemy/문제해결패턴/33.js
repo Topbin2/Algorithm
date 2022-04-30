bf57557bb51de7ff611f6719fcf8ee784a0be1ca
SANGBIN <mosangbin94@gmail.com>
2022-05-01 01:07:40 +0900

    33

--- Udemy/문제해결패턴/33.js
+++ Udemy/문제해결패턴/33.js
@@ -1,16 +1,42 @@
-const maxSubarraySum = (arr, num) => {
-  let result = 0;
-  let max = 0;
-  for (let i = 0; i < arr.length - num; i++) {
-    result = 0;
-    for (let j = 0; j < num; j++) {
-      result += arr[i + j];
-    }
-    if (max < result) {
-      max = result;
-    }
-  }
-  return max;
-};
+// const maxSubarraySum = (arr, num) => {
+//   if(arr.length === 0) return null;
 
-console.log(maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
+//   let max = Number.MIN_SAFE_INTEGER;
+//   let sum = 0;
+
+//   for(let i = 0; i < arr.length - num + 1; i++) {
+//     sum = 0;
+//     for(let j = 0; j < num; j++) {
+//       sum += arr[i + j];
+//       if(sum > max) {
+//         max = sum;
+//       }
+//     }
+//   }
+//   return max;
+// };
+
+function solution(arr, num) {
+  if(arr.length === 0) return null;
+
+  let maxSum = 0;
+  let tempSum = Number.MIN_SAFE_INTEGER;
+
+  for(let i = 0; i < num; i++) {
+    maxSum += arr[i]; //4
+  }
+  tempSum = maxSum;
+  for(let i = 0; i < arr.length - num; i++) {
+    tempSum = tempSum - arr[i] + arr[i + num];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  
+  return maxSum;
+}
+
+console.log(solution([1,2,5,2,8,1,5], 2)); // 10 , 6번 반복 -> arr.length - num + 1
+console.log(solution([1,2,5,2,8,1,5], 4)); // 17, 4번 반복
+console.log(solution([4,2,1,6], 1)); // 6, 4번 반복
+console.log(solution([4,2,1,6,2], 4)); // 13, 2번 반복
+console.log(solution([], 4)); // null
+
