3c2f7041ed2298e6818f56ecd36d0a7c8b2c4438
SANGBIN <mosangbin94@gmail.com>
2022-04-30 22:56:48 +0900

    문자열

--- Udemy/문제해결패턴/28.js
+++ Udemy/문제해결패턴/28.js
@@ -16,25 +16,42 @@
 //   return true;
 // }
 
+// function solution(str1, str2) {
+//   if(str1.length !== str2.length) return false;
+
+//   let obj1 = {};
+//   let obj2 = {};
+
+//   for(let x of str1) {
+//     obj1[x] = (obj1[x] || 0) + 1;
+//   }
+//   for(let x of str2) {
+//     obj2[x] = (obj2[x] || 0) + 1;
+//   }
+
+//   console.log(obj1)
+//   console.log(obj2)
+//   for(let key in obj1) {
+//     if(!(key in obj2)) return false;
+//     if(obj1[key] !== obj2[key]) return false;
+//   }
+//   return true;
+// }
+
 function solution(str1, str2) {
   if(str1.length !== str2.length) return false;
 
-  let obj1 = {};
-  let obj2 = {};
+  let obj = {};
 
-  for(let x of str1) {
-    obj1[x] = (obj1[x] || 0) + 1;
+  for(let i = 0; i < str1.length; i++) {
+    obj[str1[i]] ? obj[str1[i]] += 1 : obj[str1[i]] = 1;
   }
-  for(let x of str2) {
-    obj2[x] = (obj2[x] || 0) + 1;
+  console.log(obj);
+  for(let i = 0; i < str2.length; i++) {
+    if(!obj[str2[i]]) return false;
+    else obj[str2[i]] -= 1; // 0일때 false
   }
-
-  console.log(obj1)
-  console.log(obj2)
-  for(let key in obj1) {
-    if(!(key in obj2)) return false;
-    if(obj1[key] !== obj2[key]) return false;
-  }
+  console.log(obj);
   return true;
 }
 
@@ -47,11 +64,11 @@ function solution(str1, str2) {
 // const str1 = 'anagram';
 // const str2 = 'nagaram';
 
-// const str1 = 'rat';
-// const str2 = 'car';
+const str1 = 'caar';
+const str2 = 'carr';
 
-const str1 = 'awesome';
-const str2 = 'awesom';
+// const str1 = 'awesome';
+// const str2 = 'awesom';
 
 // const str1 = 'qwerty';
 // const str2 = 'qeywrt';
