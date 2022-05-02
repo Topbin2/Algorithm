3908f7a666d5516733ba3f4a0d5bb01a6b18e0c0
SANGBIN <mosangbin94@gmail.com>
2022-05-02 22:52:46 +0900

    복습

--- Udemy/문제해결패턴/37-7.js
+++ Udemy/문제해결패턴/37-7.js
@@ -1,8 +1,25 @@
+// function solution(arr, num) {
+//   if(arr.length < num) return null;
+
+//   let total = 0;
+  
+//   for(let i = 0; i < num; i++) {
+//     total += arr[i];
+//   }
+
+//   let currentTotal = total;
+
+//   for(let i = num; i < arr.length; i++) {
+//     currentTotal = currentTotal + arr[i] - arr[i - num];
+//     total = Math.max(total, currentTotal);
+//   }
+
+//   return total;
+// }
+
 function solution(arr, num) {
   if(arr.length < num) return null;
-
   let total = 0;
-  
   for(let i = 0; i < num; i++) {
     total += arr[i];
   }
@@ -10,10 +27,9 @@ function solution(arr, num) {
   let currentTotal = total;
 
   for(let i = num; i < arr.length; i++) {
-    currentTotal = currentTotal + arr[i] - arr[i - num];
+    currentTotal += arr[i] - arr[i - num];
     total = Math.max(total, currentTotal);
   }
-
   return total;
 }
 
