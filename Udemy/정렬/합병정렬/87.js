723521fed5d0db767165425b657e826615f36524
SANGBIN <mosangbin94@gmail.com>
2022-05-08 22:47:20 +0900

    합병정렬

--- Udemy/정렬/합병정렬/87.js
+++ Udemy/정렬/합병정렬/87.js
@@ -1,31 +1,66 @@
-function solution(arr1, arr2) {
-  let result = [];
+// function merge(arr1, arr2) {
+//   let result = [];
+//   let i = 0;
+//   let j = 0;
+
+//   while(i < arr1.length && j < arr2.length) {
+//     if(arr1[i] < arr2[j]) {
+//       result.push(arr1[i]);
+//       i++;
+//     } else {
+//       result.push(arr2[j]);
+//       j++;
+//     }
+//   }
+
+//   while(i < arr1.length) {
+//     result.push(arr1[i]);
+//     i++;
+//   }
+
+//   while(j < arr2.length) {
+//     result.push(arr2[j]);
+//     j++;
+//   }
+
+//   return result;
+// }
+
+function merge(arr1, arr2){
+  let results = [];
   let i = 0;
   let j = 0;
-
-  while(i < arr1.length && j < arr1.length) {
-    if(arr1[i] < arr2[j]) {
-      result.push(arr1[i]);
-      i++;
-    } else {
-      result.push(arr2[j]);
-      j++;
-    }
+  while(i < arr1.length && j < arr2.length){
+      if(arr2[j] > arr1[i]){
+          results.push(arr1[i]);
+          i++;
+      } else {
+          results.push(arr2[j])
+          j++;
+      }
   }
-
   while(i < arr1.length) {
-    result.push(arr1[i]);
-    i++;
+      results.push(arr1[i])
+      i++;
   }
-
   while(j < arr2.length) {
-    result.push(arr2[j]);
-    j++;
+      results.push(arr2[j])
+      j++;
   }
+  return results;
+}
+// console.log(merge([100,200], [1,2,3,5,6]));
 
-  return result;
+
+function mergeSort(arr) {
+  if(arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right)
 }
 
-console.log(solution([1,10,50], [2,14,99,100]));
+console.log(mergeSort([10,24,76,73]));
+console.log(mergeSort([10,24,76,73,72,1,9]));
 
-// 1, 2, 10, 14, 50
+
