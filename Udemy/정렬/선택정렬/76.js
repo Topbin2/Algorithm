1517a0e3721d7cdbd7a262c1ffe004ae4dd68ea3
SANGBIN <mosangbin94@gmail.com>
2022-05-08 19:09:37 +0900

    select sort

---
+++ Udemy/정렬/선택정렬/76.js
@@ -0,0 +1,38 @@
+// function solution(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     let minIndex = i;
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[minIndex] > arr[j]) {
+//         minIndex = j;
+//       }
+//     }
+//     if (i !== minIndex) {
+//       let temp = arr[i];
+//       arr[i] = arr[minIndex];
+//       arr[minIndex] = temp;
+//     }
+//   }
+//   return arr;
+// }
+
+function solution(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      swap(arr, minIndex, i);
+    }
+  }
+
+  return arr;
+}
+
+console.log(solution([34, 22, 10, 19, 17]));
