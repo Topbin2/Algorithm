27efbf58f28b5ca287520f69cb2c5ab2b47ef18b
SANGBIN <mosangbin94@gmail.com>
2022-05-11 10:29:39 +0900

    내적

---
+++ Programmers/Level1/내적/1.js
@@ -0,0 +1,10 @@
+function solution(a, b) {
+  let result = 0;
+  for(let i = 0; i < a.length; i++) {
+    result += a[i] * b[i];
+  }
+  return result;
+}
+
+console.log(solution([1,2,3,4], [-3,-1,0,2])); // 3
+console.log(solution([-1,0,1], [1,0,-1])); // -2
