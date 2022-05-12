701798d56a75c4a7ab3ff51dd5e7b0969cc4309a
SANGBIN <mosangbin94@gmail.com>
2022-05-12 14:48:45 +0900

    pop

--- Udemy/자료구조/단일연결리스트/1.js
+++ Udemy/자료구조/단일연결리스트/1.js
@@ -1,30 +1,3 @@
-// class Node{
-//   constructor(val){
-//       this.val = val;
-//       this.next = null;
-//   }
-// }
-
-// class SinglyLinkedList {
-//   constructor() {
-//     this.head = null;
-//     this.tail = null;
-//     this.length = 0;
-//   }
-
-//   push(val) {
-//     const newNode = new Node(val);
-//     if(!this.head) {
-//       this.head = newNode;
-//       this.tail = this.head;
-//     } else {
-//       this.tail.next = newNode;
-//       this.tail = newNode;
-//     }
-//     this.length++;
-//     return this;
-//   }
-// }
 
 class Node {
   constructor(val) {
@@ -52,12 +25,30 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if(!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while(current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if(this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
 
 const list = new SinglyLinkedList();
 list.push("Hello")
-list.push("Sangbin")
 list.push("Good bye")
+list.push("!")
 
-console.log(list.tail);
-// console.log(list);
+
+console.log(list.pop());
