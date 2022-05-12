83441260d50b8b6c6dbff516433c91fc47307c4e
SANGBIN <mosangbin94@gmail.com>
2022-05-12 14:16:16 +0900

    push

---
+++ Udemy/자료구조/단일연결리스트/1.js
@@ -0,0 +1,63 @@
+// class Node{
+//   constructor(val){
+//       this.val = val;
+//       this.next = null;
+//   }
+// }
+
+// class SinglyLinkedList {
+//   constructor() {
+//     this.head = null;
+//     this.tail = null;
+//     this.length = 0;
+//   }
+
+//   push(val) {
+//     const newNode = new Node(val);
+//     if(!this.head) {
+//       this.head = newNode;
+//       this.tail = this.head;
+//     } else {
+//       this.tail.next = newNode;
+//       this.tail = newNode;
+//     }
+//     this.length++;
+//     return this;
+//   }
+// }
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val)
+    if(!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+const list = new SinglyLinkedList();
+list.push("Hello")
+list.push("Sangbin")
+list.push("Good bye")
+
+console.log(list.tail);
+// console.log(list);
