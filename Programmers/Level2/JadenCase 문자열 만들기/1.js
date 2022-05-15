abc88e0da775077799bc3a29c784397c13b2ab52
SANGBIN <mosangbin94@gmail.com>
2022-05-16 00:09:55 +0900

    1.js

--- Programmers/Level2/JadenCase 문자열 만들기/1.js
+++ Programmers/Level2/JadenCase 문자열 만들기/1.js
@@ -1,18 +1,27 @@
 function solution(s) {
-  s = s.toLowerCase().split(' ');
+  // s = s.toLowerCase().split(' ');
 
-  let result = [];
+  // let result = [];
+
+  // for(let i = 0; i < s.length; i++) {
+  //   let str = '';
+  //   for(let j = 0; j < s[i].length; j++) {
+  //     if(j === 0) str += s[i][j].toUpperCase();
+  //     else str += s[i][j];
+  //   }
+  //   result.push(str);
+  // }
+
+  // return result.join(' ');
+
+  let answer = '';
 
   for(let i = 0; i < s.length; i++) {
-    let str = '';
-    for(let j = 0; j < s[i].length; j++) {
-      if(j === 0) str += s[i][j].toUpperCase();
-      else str += s[i][j];
-    }
-    result.push(str);
+    if(i === 0 || s[i - 1] === ' ') answer += s[i].toUpperCase();
+    else answer += s[i].toLowerCase();
   }
 
-  return result.join(' ');
+  return answer;
 }
 
 console.log(solution("3people unFollowed me")); // "3people Unfollowed Me"
