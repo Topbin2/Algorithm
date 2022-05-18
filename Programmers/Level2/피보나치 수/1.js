6c68014c1ab739454db8205d7288764da0ea7d37
SANGBIN <mosangbin94@gmail.com>
2022-05-18 16:10:39 +0900

    1.js

---
+++ Programmers/Level2/피보나치 수/1.js
@@ -0,0 +1,12 @@
+function solution(n) {
+  let answer = [0,1];
+
+  for(let i = 2; i <= n; i++) {
+    answer.push((answer[i - 1] + answer[i - 2]) % 1234567);
+  }
+
+  return answer[n];
+}
+
+console.log(solution(3)); //2
+console.log(solution(5)); //5
