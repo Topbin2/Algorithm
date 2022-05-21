1742e0f8d20c2593ff6ed54614d00d582cec7c62
SANGBIN <mosangbin94@gmail.com>
2022-05-21 20:38:47 +0900

    1.js

--- Programmers/Level2/최솟값 만들기/1.js
+++ Programmers/Level2/최솟값 만들기/1.js
@@ -2,12 +2,14 @@ function solution(A, B) {
   A.sort((a, b) => a - b);
   B.sort((a, b) => b - a);
 
-  let result = 0;
-  for(let i = 0; i < A.length; i++) {
-    result += A[i] * B[i];
-  }
+  // let result = 0;
+  // for(let i = 0; i < A.length; i++) {
+  //   result += A[i] * B[i];
+  // }
 
-  return result;
+  // return result;
+
+  return A.reduce((total, val, idx) => total + val * B[idx], 0);
 }
 
 console.log(solution([1,4,2], [5,4,4])); // 29
