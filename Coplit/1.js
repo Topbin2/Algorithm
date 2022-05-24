568a5affe1e5fd77f7bc21df75198494daab226d
SANGBIN <mosangbin94@gmail.com>
2022-05-24 18:56:25 +0900

    1.js

---
+++ Coplit/1.js
@@ -0,0 +1,19 @@
+function transformFirstAndLast(arr) {
+  // TODO: 여기에 코드를 작성합니다.
+
+  let result = {};
+
+  if(arr.length === 0) return {};
+  else {
+    result[arr[0]] = arr[arr.length - 1];
+    return result;
+  }
+}
+
+let arr = ['Queen', 'Elizabeth', 'Of Hearts', 'Beyonce'];
+let output = transformFirstAndLast(arr);
+console.log(output); // --> { Queen : 'Beyonce' }
+
+arr = ['Kevin', 'Bacon', 'Love', 'Hart', 'Costner', 'Spacey'];
+output = transformFirstAndLast(arr);
+console.log(output); // --> { Kevin : 'Spacey' }
