d514ce02d54e958bd52531c5852626f3fa9b0665
SANGBIN <mosangbin94@gmail.com>
2022-05-25 16:27:05 +0900

    6.js

---
+++ Coplit/6.js
@@ -0,0 +1,15 @@
+function letterCapitalize(str) {
+  // TODO: 여기에 코드를 작성합니다.
+
+  let result = '';
+
+  for(let i = 0; i < str.length; i++) {
+    if(i === 0) result += str[i].toUpperCase();
+    
+  }
+}
+
+let output1 = letterCapitalize('hello world');
+console.log(output1); // "Hello World"
+let output2 = letterCapitalize('javascript  is sexy ');
+console.log(output2); // "Javascript  Is Sexy "
