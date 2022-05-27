335a18d188efff0f03e2188ce9dd22bef91897f9
MoSangBin <mosangbin94@gmail.com>
2022-05-27 18:34:48 +0900

    1.js

--- Practice/1.js
+++ Practice/1.js
@@ -1,18 +1,43 @@
-function computeWhenDouble(interestRate) {
+const each = function (collection, iteratee) {
   // TODO: 여기에 코드를 작성합니다.
 
-  let initialValue = 100;
-  let year = 0;
-
-  while(initialValue < 200) {
-    initialValue += initialValue * (interestRate * 0.01);
-    year++; 
+  if(Array.isArray(collection)) {
+    for(let i = 0; i < collection.length; i++) {
+      iteratee(collection[i], i, collection)
+    }
+  } else {
+    for(let key in collection) {
+      iteratee(collection[key], key, collection);
+    }
   }
+};
 
-  return year;
+const filter = function (arr, test) {
+  // TODO: 여기에 코드를 작성합니다.
+  let result = [];
+
+  each(arr, (item) => {
+    if(test(item)) result.push(item);
+  })
+
+  return result;
+};
+
+const reject = function (arr, test) {
+  // TODO: 여기에 코드를 작성합니다.
+  // TIP: 위에서 구현한 `filter` 함수를 사용해서 `reject` 함수를 구현해 보세요.
+  let result = [];
+
+  filter(arr, (item) => {
+    if(!test(item)) result.push(item);
+  })
+
+  return result;
+};
+
+function test(item) {
+  if(item % 2 === 0) return true;
+  else return false;
 }
 
-console.log(computeWhenDouble(1)); // 70
-console.log(computeWhenDouble(2.3)); // 31
-console.log(computeWhenDouble(7)); // 11
-console.log(computeWhenDouble(100)); // 1
+reject([1,2,3,4,5], (item) => test(item));
