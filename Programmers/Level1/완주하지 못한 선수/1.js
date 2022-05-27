180d69d410ce252b4e7be7b372fc185e4c07ea92
MoSangBin <mosangbin94@gmail.com>
2022-05-27 19:01:56 +0900

    1.js

--- Programmers/Level1/완주하지 못한 선수/1.js
+++ Programmers/Level1/완주하지 못한 선수/1.js
@@ -1,3 +1,21 @@
 function solution(participant, completion) {
-    
+  let result = "";
+
+  participant.sort();
+  completion.sort();
+
+  for (let i = 0; i < participant.length; i++) {
+    if(participant[i] !== completion[i]) return participant[i];
+  }
 }
+
+console.log(solution(["leo", "kiki", "eden"], ["eden", "kiki"])); // 'leo'
+console.log(
+  solution(
+    ["marina", "josipa", "nikola", "vinko", "filipa"],
+    ["josipa", "filipa", "marina", "nikola"]
+  )
+); // 'vinko'
+console.log(
+  solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"])
+); // 'mislav'
