2d70de7420154069282805facad17b7e415d7423
MoSangBin <mosangbin94@gmail.com>
2022-05-29 15:44:26 +0900

    1.js

--- Programmers/Level1/숫자 문자열과 영단어/1.js
+++ Programmers/Level1/숫자 문자열과 영단어/1.js
@@ -30,7 +30,21 @@ function solution(s) {
   return Number(result);
 }
 
+// function solution(s) {
+//   let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+//   var answer = s;
+
+//   for(let i=0; i< numbers.length; i++) {
+//       let arr = answer.split(numbers[i]);
+//       console.log(arr);
+//       answer = arr.join(i);
+//       console.log(answer);
+//   }
+
+//   return Number(answer);
+// }
+
 console.log(solution("one4seveneight")); // 1478
-console.log(solution("23four5six7")); // 234567
-console.log(solution("2three45sixseven")); // 234567
-console.log(solution("123")); // 123
+// console.log(solution("23four5six7")); // 234567
+// console.log(solution("2three45sixseven")); // 234567
+// console.log(solution("123")); // 123
