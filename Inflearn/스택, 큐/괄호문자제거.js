6b5f5b73fddd99e9340d0056fcd2f9af131a4365
MoSangBin <mosangbin94@gmail.com>
2022-06-01 22:21:05 +0900

    stack

--- Inflearn/스택, 큐/괄호문자제거.js
+++ Inflearn/스택, 큐/괄호문자제거.js
@@ -1,20 +1,34 @@
+// function solution(s){  
+//   const stack = [];
+
+//   for(let x of s) {
+
+//     if(x === ')') {
+//       while(stack[stack.length - 1] !== '(') {
+//         stack.pop();
+//       }
+//       stack.pop();
+//     } else {
+//       stack.push(x);
+//     }
+
+//   }
+
+//   return stack.join('');
+// }
+
 function solution(s){  
+
   const stack = [];
+  let result = '';
 
   for(let x of s) {
-
-    if(x === ')') {
-      while(stack[stack.length - 1] !== '(') {
-        stack.pop();
-      }
-      stack.pop();
-    } else {
-      stack.push(x);
-    }
-
+    if(x === '(') stack.push(x);
+    else if(x === ')') stack.pop();
+    else if(stack.length === 0) result += x;
   }
 
-  return stack.join('');
+  return result;
 }
 
 let str="(A(BC)D)EF(G(H)(IJ)K)LM(N)";
