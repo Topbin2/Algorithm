b5002292f8c2db8a30f2fbf2b3996d8bd797a307
MoSangBin <mosangbin94@gmail.com>
2022-06-07 10:04:16 +0900

    1.js

--- Programmers/Level1/체육복/1.js
+++ Programmers/Level1/체육복/1.js
@@ -1,34 +1,43 @@
 function solution(n, lost, reserve) {
-  const students = [];
-
-  for(let i = 1; i <= n; i++) {
-    students.push(1);
+  let answer = 0;
+  const uniform = [];
+  
+  // 0) 모두가 체육복을 1벌씩 가지고 있도록 설정
+  for (let i = 0; i < n; i++) {
+      uniform[i] = 1;
   }
-
-  for(let i = 0; i < lost.length; i++) {
-    students[lost[i] - 1] = 0;
+  
+  // 1) 체육복을 도난당한 학생의 체육복을 0개로 수정
+  for (let i = 0; i < lost.length; i++) {    
+      uniform[lost[i]-1] = 0;
   }
-
-  for(let i = 0; i < reserve.length; i++) {
-    students[reserve[i] - 1] += 1;
+  
+  // 2) 여벌의 체육복을 가져온 학생의 체육복을 +1개로 수정
+  for (let i = 0; i < reserve.length; i++) {
+      uniform[reserve[i]-1] += 1;
   }
-
-  for(let i = 1; i < students.length; i++) {
-    if(students[i] === 0 && students[i - 1] === 2) {
-      students[i] = 1;
-      students[i - 1] = 1;
-    }
+  
+  // 3-1) 체육복이 0개인 학생이 앞번호에서 체육복을 빌려올 수 있는 경우
+  for (let i = 0; i < n; i++) {
+      if (uniform[i-1] === 2 && uniform[i] === 0) {
+          uniform[i-1] = 1;
+          uniform[i] = 1;
+  
+  // 3-2) 체육복이 0개인 학생이 뒷번호에서 체육복을 빌려올 수 있는 경우
+      } else if (uniform[i] === 0 && uniform[i+1] === 2) {
+          uniform[i] = 1;
+          uniform[i+1] = 1;
+      }
   }
-
-  for(let i = 0; i < students.length - 1; i++) {
-    if(students[i] === 0 && students[i + 1] === 2) {
-      students[i] = 1;
-      students[i + 1] = 1;
-    }
+  
+  // 결과: 체육복을 1개 이상 가지고 있게 된 학생의 수를 카운트 후, 답안 제출
+  for (let i = 0; i < n; i++) {
+      if (uniform[i] > 0) {
+          answer++;
+      }
   }
-
-  return students.filter(el => el !== 0).length;
-
+  
+  return answer;
 }
 
 // 1. 모든 학생 체육복 하나씩(1) 갖는 배열 생성
