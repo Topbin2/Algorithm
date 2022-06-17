702c5d6f8cc710c22000f84988666cd7148152b2
MoSangBin <mosangbin94@gmail.com>
2022-06-17 10:04:45 +0900

    1

---
+++ Backjoon/b7568/app.js
@@ -0,0 +1,24 @@
+let input = require("fs")
+  .readFileSync("./input.txt")
+  .toString()
+  .split("\n")
+  .map((item) => item.split(" ").map((item) => Number(item)))
+  .slice(1);
+
+console.log(input);
+
+let rank = 1;
+const result = [];
+
+for (let i = 0; i < input.length; i++) {
+  rank = 1;
+  for (let j = 0; j < input.length; j++) {
+    if (i === j) continue;
+    else {
+      if (input[i][0] < input[j][0] && input[i][1] < input[j][1]) rank++;
+    }
+  }
+  result.push(rank);
+}
+
+console.log(result.join(" ").trim());
