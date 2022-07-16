c70a3e21d94336b22d849885e55988a49ccc2715
MoSangBin <mosangbin94@gmail.com>
2022-07-16 17:15:50 +0900

    ts

--- Udemy/이진 힙/PriorityQueue.js
+++ Udemy/이진 힙/PriorityQueue.js
@@ -1,10 +1,11 @@
-class MaxBinaryHeap {
+class PriorityQueue {
   constructor() {
-    this.values = [41, 39, 33, 18, 27, 12];
+    this.values = [];
   }
 
-  insert(element) {
-    this.values.push(element);
+  enqueue(val, priority) {
+    const newNode = new Node(val, priority);
+    this.values.push(newNode);
     this.bubbleUp();
   }
 
@@ -15,22 +16,22 @@ class MaxBinaryHeap {
     while (idx > 0) {
       let parentIdx = Math.floor((idx - 1) / 2);
       let parent = this.values[parentIdx];
-      if (element <= parent) break;
+      if (element.priority >= parent.priority) break;
       this.values[parentIdx] = element;
       this.values[idx] = parent;
       idx = parentIdx;
     }
   }
 
-  extractMax() {
-    const max = this.values[0];
+  dequeue() {
+    const min = this.values[0];
     const end = this.values.pop();
     if (this.values.length > 0) {
       this.values[0] = end;
       this.sinkDown();
     }
 
-    return max;
+    return min;
   }
 
   sinkDown() {
@@ -46,7 +47,7 @@ class MaxBinaryHeap {
 
       if (leftChildIdx < length) {
         leftChild = this.values[leftChildIdx];
-        if (leftChild > element) {
+        if (leftChild.priority < element.priority) {
           swap = leftChildIdx;
         }
       }
@@ -54,8 +55,8 @@ class MaxBinaryHeap {
       if (rightChildIdx < length) {
         rightChild = this.values[rightChildIdx];
         if (
-          (swap === null && rightChild > element) ||
-          (swap !== null && rightChild > leftChild)
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChild.priority)
         ) {
           swap = rightChildIdx;
         }
@@ -69,25 +70,18 @@ class MaxBinaryHeap {
   }
 }
 
-let heap = new MaxBinaryHeap();
-heap.insert(55);
-heap.insert(45);
-heap.insert(1);
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
 
-heap.extractMax();
-heap.extractMax();
-heap.extractMax();
-heap.extractMax();
-heap.extractMax();
-heap.extractMax();
-heap.extractMax();
-heap.extractMax();
-heap.extractMax();
-heap.extractMax();
+let ER = new PriorityQueue();
+ER.enqueue("common cold", 5);
+ER.enqueue("gunshot wound", 1);
+ER.enqueue("high fever", 4);
+ER.enqueue("broken arm", 2);
+ER.enqueue("glass in foot", 3);
 
-console.log(heap.values);
-
-// BigO
-// Insertion - O(lon N)
-// Removal - O(lon N)
-// Search = O(N)
+console.log(ER);
