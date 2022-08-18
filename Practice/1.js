51ec3730cd702d8f5f20996b6867b6290900b63c
MoSangBin <mosangbin94@gmail.com>
2022-08-18 20:15:10 +0900

    practice

--- Practice/1.js
+++ Practice/1.js
@@ -1,23 +1,31 @@
-function solution(test){
-  let answer=0;
-  m=test.length;
-  n=test[0].length;
-  for(let i=1; i<=n; i++){
-      for(let j=1; j<=n; j++){
-          let cnt=0;
-          for(let k=0; k<m; k++){
-              let pi=pj=0;
-              for(let s=0; s<n; s++){
-                  if(test[k][s]===i) pi=s;
-                  if(test[k][s]===j) pj=s;
-              }
-              if(pi<pj) cnt++;
-          }
-          if(cnt===m) answer++;
-      }
+function solution(arr1, arr2) {
+  const result = new Array(arr2.length).fill(0);
+ 
+  // [0, 0, 0, 0, 0, 0, 0, 0,]
+
+  arr1.sort((a, b) => a - b);
+  
+  for(let i = 0; i < arr2.length; i++) {
+    if(search(arr2[i], arr1)) result[i] = 1;
   }
-  return answer;
+
+  return result;
 }
 
-let arr=[[3, 4, 1, 2], [4, 3, 2, 1], [3, 1, 4, 2]];
-console.log(solution(arr));
+function search(num, arr) {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while(start <= end) {
+    const mid = Math.floor((start + end) / 2);
+    if(arr[mid] === num) return true;
+    else if(arr[mid] < num) start = mid + 1;
+    else if(arr[mid] > num) end = mid - 1;
+  }
+
+  return false;
+}
+
+console.log(solution([6,3,2,10,-10], [10, 9, -5, 2, 3, 4, 5, -10])); // 1 0 0 1 1 0 0 1
+
+
