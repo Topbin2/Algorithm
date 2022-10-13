62c61dbaeadb351ed9722e340ac9bd939443b624
MoSangBin <mosangbin94@gmail.com>
2022-10-13 22:59:11 +0900

    practice

--- Practice/1.js
+++ Practice/1.js
@@ -1,31 +1,61 @@
-function solution(arr1, arr2) {
-  const result = new Array(arr2.length).fill(0);
- 
-  // [0, 0, 0, 0, 0, 0, 0, 0,]
 
-  arr1.sort((a, b) => a - b);
-  
-  for(let i = 0; i < arr2.length; i++) {
-    if(search(arr2[i], arr1)) result[i] = 1;
+function solution(n, plans, clients) {
+  const arr = new Array(clients.length).fill(0);
+
+  const info = []; 
+
+  plans = plans.map((arr) => arr.split(' '));
+
+  let accService = [];
+  for(let i = 0; i < plans.length; i++) {
+      const obj = {
+              number: i + 1,
+              data: Number(plans[i][0]),
+              service: [],
+              } 
+
+      for(let j = 1; j < plans[i].length; j++) {
+            accService.push(plans[i][j]);
+      }
+      obj.service = [...accService];
+      info.push(obj);
   }
 
-  return result;
-}
+  clients = clients.map((arr) => arr.split(' '));
 
-function search(num, arr) {
-  let start = 0;
-  let end = arr.length - 1;
+  const consumer = [];
 
-  while(start <= end) {
-    const mid = Math.floor((start + end) / 2);
-    if(arr[mid] === num) return true;
-    else if(arr[mid] < num) start = mid + 1;
-    else if(arr[mid] > num) end = mid - 1;
+  for(let i = 0; i < clients.length; i++) {
+          const obj = {
+              data: Number(clients[i][0]),
+              service: [...clients[i].slice(1)],
+          }
+          consumer.push(obj);
   }
 
-  return false;
+  for(let i = 0; i < consumer.length; i++) {
+    for(let j = 0; j < info.length; j++) {
+      if(consumer[i].data < info[j].data) {
+        for(let k = 0; k < consumer[i].service.length; k++) {
+          if(!info[j].service.includes(consumer[i].service[k])) break;
+          if(k === consumer[i].service.length - 1) arr[i] = info[j].number;
+        }
+      }
+    }
+  }
+
+  return arr;
 }
 
-console.log(solution([6,3,2,10,-10], [10, 9, -5, 2, 3, 4, 5, -10]));
+[
+  { number: 1, data: 100, service: [ '1', '3' ] },
+  { number: 2, data: 500, service: [ '1', '3', '4' ] },
+  { number: 3, data: 2000, service: [ '1', '3', '4', '5' ] }
+] [
+  { data: 300, service: [ '3', '5' ] },
+  { data: 1500, service: [ '1' ] },
+  { data: 100, service: [ '1', '3' ] },
+  { data: 50, service: [ '1', '2' ] }
+]
 
-
+console.log(solution(3, ['100 1 3', '500 4', '2000 5'], ['300 3 5', '1500 1', '100 1 3', '50 1 2']));
