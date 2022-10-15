d90541aaeaf0c1d36b9ac1bad7de1173604ce4df
MoSangBin <mosangbin94@gmail.com>
2022-10-15 23:46:51 +0900

    practice

--- Practice/1.js
+++ Practice/1.js
@@ -1,21 +1,17 @@
-function solution(m, product){
-  let answer=0;
-  let n=product.length;
-  product.sort((a, b)=>(a[0]+a[1])-(b[0]+b[1]));
-  for(let i=0; i<n; i++){
-      let money=m-(product[i][0]/2+product[i][1]);
-      let cnt=1;
-      for(let j=0; j<n; j++){
-          if(j!==i && (product[j][0]+product[j][1])>money) break;
-          if(j!==i && (product[j][0]+product[j][1])<=money){
-              money-=(product[j][0]+product[j][1]);
-              cnt++;
-          }
-      }
-      answer=Math.max(answer, cnt);
-  }  
-  return answer;
-}
-
-let arr=[[6, 6], [2, 2], [4, 3], [4, 5], [10, 3]];
-console.log(solution(28, arr));
+function solution(arr) {
+    let result = [];
+  
+    function helper(helperInput) {
+      if(helperInput.length === 0) return;
+  
+      if(helperInput[0] % 2 !== 0) result.push(helperInput[0]);
+  
+      helper(helperInput.slice(1));
+    }
+  
+    helper(arr);
+  
+    return result;
+  }
+  
+  console.log(solution([1,2,3,4,5,6,7,8,9]));
