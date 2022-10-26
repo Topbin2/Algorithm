8e243b3a6b259d015f45c496c59bdc9318dfeba0
MoSangBin <mosangbin94@gmail.com>
2022-10-26 23:42:55 +0900

    practice

--- Practice/1.js
+++ Practice/1.js
@@ -1,23 +1,16 @@
-function solution(n) {
+function solution(arr, divisor) {
+  // var answer = [];
+  // for(let x of arr) {
+  //   if(x % divisor === 0) answer.push(x);
+  // }
+  // if(!answer.length) return [-1];
+  // answer.sort((a, b) => a - b);
+  // return answer;
 
-  let result = 0;
-
-  for(let i = 1; i <= n; i++) {
-    for(let j = i, sum = 0; j <= n; j++) {
-      sum += j;
-
-      if(sum === n) {
-        result++;
-      }
-
-      if(sum > n) {
-        break;
-      }
-
-    }
-  }
-
-  return result;
+  let answer = arr.filter(num => num % divisor === 0);
+  return answer.length === 0 ? [-1] : answer.sort((a, b) => a - b);
 }
 
-console.log(solution(15)); // 4
+console.log(solution([5,9,7,10], 5)); // [5,10]
+console.log(solution([2,36,1,3], 1)); // [1,2,3,36]
+console.log(solution([3,2,6], 10)); // -1
